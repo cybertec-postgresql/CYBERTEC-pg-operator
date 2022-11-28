2214ab4550e398842f1b761646d8fe467423c0fa
Igor Yanchenko <yanchenko.igor@gmail.com>
2022-11-28 14:21:21 +0200

    create cronjob before creating the statefulset

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -313,6 +313,14 @@ func (c *Cluster) Create() (err error) {
 	}
 	c.logger.Infof("pod disruption budget %q has been successfully created", util.NameFromMeta(pdb.ObjectMeta))
 
+	if c.Postgresql.Spec.Backup.Pgbackrest != nil {
+		if err = c.syncPgbackrestConfig(); err != nil {
+			err = fmt.Errorf("could not sync pgbackrest config: %v", err)
+			return err
+		}
+		c.logger.Info("a pgbackrest config has been successfully synced")
+	}
+
 	if c.Statefulset != nil {
 		return fmt.Errorf("statefulset already exists in the cluster")
 	}
@@ -362,16 +370,6 @@ func (c *Cluster) Create() (err error) {
 			return fmt.Errorf("could not create a k8s cron job for pgbackrest: %v", err)
 		}
 		c.logger.Info("a k8s cron job for pgbackrest has been successfully created")
-
-		if err = c.syncPgbackrestConfig(); err != nil {
-			err = fmt.Errorf("could not sync pgbackrest config: %v", err)
-			return err
-		}
-		c.logger.Info("a pgbackrest config has been successfully synced")
-		if err = c.syncPgbackrestConfig(); err != nil {
-			err = fmt.Errorf("could not sync pgbackrest config: %v", err)
-			return err
-		}
 	}
 
 	if err := c.listResources(); err != nil {

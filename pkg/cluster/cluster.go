60f34daf0034c6618894588c39bfc24712539406
rafia sabih <rafia.sabih@cybertec.at>
2024-04-04 13:01:26 +0530

    Minor fixes + cosmetic changes

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -165,7 +165,6 @@ func New(cfg Config, kubeClient k8sutil.KubernetesClient, pgSpec cpov1.Postgresq
 
 	//Check if monitoring user is added in manifest
 	if _, ok := pgSpec.Spec.Users["cpo-exporter"]; ok {
-
 		cluster.logger.Error("creating user of name cpo-exporter is not allowed as it is reserved for monitoring")
 	}
 	return cluster
@@ -981,6 +980,10 @@ func (c *Cluster) Update(oldSpec, newSpec *cpov1.Postgresql) error {
 			newSpec.Spec.Users = users
 			newSpec.Spec.Users[monitorUsername] = flg
 		}
+	}
+	//Check if monitoring user is added in manifest
+	if _, ok := newSpec.Spec.Users["cpo-exporter"]; ok {
+		c.logger.Error("creating user of name cpo-exporter is not allowed as it is reserved for monitoring")
 	}
 
 	// Users
--- pkg/cluster/sync.go
+++ pkg/cluster/sync.go
@@ -1243,6 +1243,11 @@ func (c *Cluster) syncRoles() (err error) {
 		}
 	}()
 
+	//Check if monitoring user is added in manifest
+	if _, ok := c.Spec.Users["cpo-exporter"]; ok {
+		c.logger.Error("creating user of name cpo-exporter is not allowed as it is reserved for monitoring")
+	}
+
 	// mapping between original role name and with deletion suffix
 	deletedUsers := map[string]string{}
 	newUsers = make(map[string]spec.PgUser)
